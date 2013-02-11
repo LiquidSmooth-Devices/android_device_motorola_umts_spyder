aa34c78ca453b6da59dacc378c426c42df2bfa81
dhacker29 <davidhackerdvm@gmail.com>
2013-02-11 01:09:31 -0600

    Liquify

---
+++ liquid.mk
@@ -0,0 +1,37 @@
+# Inherit device configuration for VZW Droid RAZR.
+$(call inherit-product, device/motorola/umts_spyder/full_umts_spyder.mk)
+
+# Inherit some common liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Inherit some common GSM stuff.
+$(call inherit-product, vendor/liquid/config/gsm.mk)
+
+# CM-specific init file for SafeStrap
+PRODUCT_COPY_FILES += \
+    vendor/liquid/prebuilt/common/etc/init.liquid.rc:system/etc/rootfs/init.liquid.rc
+
+#
+# Setup device specific product configuration.
+#
+PRODUCT_NAME := liquid_umts_spyder
+PRODUCT_BRAND := RTGB
+PRODUCT_DEVICE := umts_spyder
+PRODUCT_MODEL := XT910
+PRODUCT_MANUFACTURER := MOTO
+PRODUCT_RELEASE_NAME := MOTOROLA RAZR
+PRODUCT_SFX := umts
+
+UTC_DATE := $(shell date +%s)
+DATE := $(shell date +%Y%m%d)
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+   PRODUCT_NAME=${PRODUCT_DEVICE}_${PRODUCT_SFX} \
+   BUILD_NUMBER=${DATE} \
+   TARGET_DEVICE=${PRODUCT_DEVICE} \
+   BUILD_FINGERPRINT=${PRODUCT_BRAND}/${PRODUCT_DEVICE}_${PRODUCT_SFX}/${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}:${PLATFORM_VERSION}/${BUILD_ID}/${DATE}:user/release-keys \
+   PRIVATE_BUILD_DESC="${PRODUCT_DEVICE_PREFIX}_${PRODUCT_DEVICE}-user ${PLATFORM_VERSION} ${BUILD_ID} ${DATE} release-keys" \
+   PRODUCT_BRAND=${PRODUCT_BRAND} \
+   BUILD_UTC_DATE= \
+   PRODUCT_DEFAULT_LANGUAGE=en \
+   PRODUCT_DEFAULT_REGION=US \
